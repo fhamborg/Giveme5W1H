99b676bbe78f8ecccf0946e63140ce465688c105
soeren <nereos@mind-funk.de>
2017-04-24 21:21:53 +0200

    refined NP-VP-VP patterns

--- extractor/extractors/cause_extractor.py
+++ extractor/extractors/cause_extractor.py
@@ -1,5 +1,6 @@
 import logging
 import nltk
+from nltk.corpus import wordnet
 from nltk.stem.wordnet import WordNetLemmatizer
 from copy import deepcopy
 from .abs_extractor import AbsExtractor
@@ -11,17 +12,31 @@ class CauseExtractor(AbsExtractor):
     """
 
     # weights used in the candidate evaluation:
-    # (position, pattern type)
-    weights = (1, 1)
+    # (position, pattern types)
+    weights = (1, 1, 1, 1)
 
-    # list of indicators indicating a cause-effect relation
-    # this list is inspired by TODO
-    verb_indicators = ['allow', 'affect', 'alter', 'arouse', 'cause', 'change', 'create', 'criticize', 'convert',
-                       'cut', 'develop', 'derive', 'encourage', 'extend', 'force', 'let', 'permit', 'make', 'wash',
-                       'generate', 'result']
     adverbial_indicators = ['therefore', 'hence', 'thus', 'consequently', 'accordingly']  # 'so' has problems with JJ
     clausal_conjunctions = {'consequence': 'of', 'effect': 'of',  'result': 'of', 'upshot': 'of', 'outcome': 'of',
                             'because': '', 'due': 'to', 'stemmed': 'from'}
+
+    # list of verbs for the detection of cause-effect relations within NP-VP-NP patterns
+    # this list and the TODO
+    causal_verbs = ['activate', 'actuate', 'arouse', 'associate', 'begin', 'bring', 'call', 'cause', 'commence',
+                    'conduce', 'contribute', 'create', 'derive', 'develop', 'educe', 'effect', 'effectuate', 'elicit',
+                    'entail', 'evoke', 'fire', 'generate', 'give', 'implicate', 'induce', 'kick', 'kindle', 'launch',
+                    'lead', 'link', 'make', 'originate', 'produce', 'provoke', 'put', 'relate', 'result', 'rise', 'set',
+                    'spark', 'start', 'stem', 'stimulate', 'stir', 'trigger', 'unleash']
+
+    # list of verbs that require additional tokens
+    causal_verb_phrases = {'call': ['down', 'forth'], 'fire': ['up'], 'give': ['birth'], 'kick': ['up'],
+                           'put': ['forward'], 'set': ['in motion', 'off', 'up'], 'stir': ['up']}
+
+    # verbs involved in NP-VP-NP constraints
+    constraints_verbs = {'cause': None, 'associate': None, 'relate': None, 'lead': None, 'induce': None}
+
+    # hyponym classes involved in NP-VP-NP constraints
+    constraints_hyponyms = {'entity': None, 'phenomenon': None, 'abstraction': None, 'group': None, 'possession': None,
+                            'event': None, 'act': None, 'state': None}
 
     def __init__(self, weights=None):
         """
@@ -41,6 +56,23 @@ class CauseExtractor(AbsExtractor):
 
         if weights is not None:
             self.weights = weights
+
+        # translate causal verbs into synsets
+        synsets = []
+        for verb in self.causal_verbs:
+            synsets += wordnet.synsets(verb, 'v')
+        self.causal_verbs = set(synsets)
+
+        for verb in self.constraints_verbs:
+            self.constraints_verbs[verb] = set(wordnet.synsets(verb, 'v'))
+
+        # initialize synsets that are used as constraints in NP-VP-VP patterns
+        for noun in self.constraints_hyponyms:
+            hyponyms = set()
+            for synset in wordnet.synsets(noun, 'n'):
+                hyponyms |= self.get_hyponyms(synset)
+            self.constraints_hyponyms[noun] = hyponyms
+
 
         self.lemmatizer = WordNetLemmatizer()
 
@@ -97,24 +129,85 @@ class CauseExtractor(AbsExtractor):
         for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP' and t.right_sibling() is not None):
             sibling = subtree.right_sibling()
 
-            # TODO Modal auxiliaries? (MD)?
-
             # skip to the first verb
+            # TODO skip Modal auxiliaries? (MD)?
             while sibling.label() == 'ADVP' and sibling.right_sibling() is not None:
                 sibling = sibling.right_sibling()
 
+            # NP-VP-NP pattern found
             if sibling.label() == 'VP' and '(NP' in sibling.pformat():
-                leaves = sibling.leaves()
-                verb = leaves[0].lower()
+                verbs = [t[0] for t in sibling.pos() if t[1][0] == 'V'][:3]
+                verb_synset = set()
 
-                # TODO passive sentences?
+                # depending on the used tense, we may have to look at the second/third verb e.g. 'have been ...'
+                for verb in verbs:
+                    normalized = verb.lower()
 
-                if verb in self.verb_indicators:
+                    # check if word meaning is relevant
+                    verb_synset = set(wordnet.synsets(normalized, 'v'))
+                    if verb_synset.isdisjoint(self.causal_verbs):
+                        continue
+
+                    # if necessary look at the  following phrase
+                    lemma = self.lemmatizer.lemmatize(normalized)
+                    if lemma in self.causal_verb_phrases:
+                        # fetch following two tokens
+                        rest = ''
+                        for i, token in enumerate(verbs):
+                            if verb == token[0]:
+                                rest = ' '.join([t[0] for t in verbs[i+1:i+3]]).lower()
+                                break
+                        if rest != self.causal_verb_phrases[lemma]:
+                            continue
+
+                # pattern contains a valid verb, so check the 8 subpatterns
+                if not verb_synset.isdisjoint(self.constraints_verbs['cause']):
                     candidates.append(deepcopy([subtree.pos(), sibling.pos(), 'NP-VP-NP']))
-                elif self.lemmatizer.lemmatize(verb, 'v') in self.verb_indicators:
-                    # if the first attempt failed, try to lemmatize the verb using WordNet
-                    candidates.append(deepcopy([subtree.pos(), sibling.pos(), 'NP-VP-NP']))
+                else:
+                    pre = [t[0].lower() for t in subtree.pos() if t[1][0] == 'N' and t[0].isalpha()]
+                    post = [t[0].lower() for t in sibling.pos() if t[1][0] == 'N' and t[0].isalpha()]
+                    pre_con = {'entity': False, 'abstraction': False}
+                    post_con = {'entity': False, 'phenomenon': False, 'abstraction': False, 'group': False,
+                                'possession': False, 'event': False, 'act': False, 'state': False}
+                    verb_con = {'associate': False, 'relate': False, 'lead': False, 'induce': False}
 
+                    # check nouns in after verb
+                    for noun in post:
+                        noun_synset = set(wordnet.synsets(noun, 'n'))
+                        for con in post_con:
+                            post_con[con] = post_con[con] or not noun_synset.isdisjoint(self.constraints_hyponyms[con])
+                            if post_con['phenomenon']:
+                                break
+
+                        if post_con['phenomenon']:
+                            break
+
+                    # check nouns in before verb
+                    for noun in pre:
+                        noun_synset = set(wordnet.synsets(noun, 'n'))
+                        for con in pre_con:
+                            pre_con[con] = pre_con[con] or not noun_synset.isdisjoint(self.constraints_hyponyms[con])
+
+                    # check if verb is relevant for a subpattern
+                    for con in verb_con:
+                        verb_con[con] = not verb_synset.isdisjoint(self.constraints_verbs[con])
+                        if verb_con[con]:
+                            break
+
+                    # apply subpatterns
+                    if post_con['phenomenon']:
+                        candidates.append(deepcopy([subtree.pos(), sibling.pos(), 'NP-VP-NP']))
+                    elif not pre_con['entity'] and (verb_con['associate'] or verb_con['relate']) and (
+                                    post_con['abstraction'] and post_con['group'] and post_con['possession']):
+                        candidates.append(deepcopy([subtree.pos(), sibling.pos(), 'NP-VP-NP']))
+                    elif not pre_con['entity'] and post_con['event']:
+                        candidates.append(deepcopy([subtree.pos(), sibling.pos(), 'NP-VP-NP']))
+                    elif not pre_con['abstraction'] and (post_con['event'] or post_con['act']):
+                        candidates.append(deepcopy([subtree.pos(), sibling.pos(), 'NP-VP-NP']))
+                    elif verb_con['lead'] and (not post_con['entity'] and not post_con['group']):
+                        candidates.append(deepcopy([subtree.pos(), sibling.pos(), 'NP-VP-NP']))
+
+        # search for adverbs or clausal conjunctions
         for i in range(len(tokens)):
             token = tokens[i].lower()
 
@@ -136,6 +229,7 @@ class CauseExtractor(AbsExtractor):
         :param document: The parsed document.
         :param candidates: The extracted candidates: [cause, effect, pattern]
         :param weights: Optional weighting used for the evaluation: [position, pattern]
+
         :return: A list of evaluated and ranked candidates
         """
 
@@ -146,13 +240,17 @@ class CauseExtractor(AbsExtractor):
             if candidate is not None and len(candidate[0]) > 0:
                 # following the concept of the inverted pyramid use the position for scoring
                 score = self.weights[0] * (document.get_len()-candidate[2]) / document.get_len()
+
                 # we also consider the pattern typ used to detect the candidate
                 if candidate[1] == 'biclausal':
                     # the most obvious candidates have biclausal indicators and get the most boost
                     score += self.weights[1]
                 elif candidate[1] == 'RB':
                     # while not as significant as biclausal indicators, adverbials are mor significant as the verbs
-                    score += self.weights[1] * 0.5
+                    score += self.weights[2]
+                else:
+                    # NP-VP-NP
+                    score += self.weights[3]
 
                 if score > 0:
                     score /= weights_sum
@@ -161,3 +259,18 @@ class CauseExtractor(AbsExtractor):
 
         ranked_candidates.sort(key=lambda x: x[1], reverse=True)
         return ranked_candidates
+
+    def get_hyponyms(self, synsets):
+        """
+        Fetches all hyponyms in a recursive manner creating a word class.
+
+        :param synsets: The list of synsets to process
+        :type synsets: [synset]
+
+        :return: A set of synsets
+        """
+
+        result = set()
+        for hyponym in synsets.hyponyms():
+            result |= self.get_hyponyms(hyponym)
+        return result | set(synsets.hyponyms())

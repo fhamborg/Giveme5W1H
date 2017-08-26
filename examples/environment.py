f0899488799203f084c1736508d4b74d5fde4edf
Benedikt Kromer <benediktkromer@gmail.com>
2017-08-26 14:31:42 +0200

    - cmd wrapper to simplify startup - startup scripts

---
+++ examples/environment.py
@@ -0,0 +1,8 @@
+from util import StartupHelper
+
+
+def start():
+    h = StartupHelper()
+    h.do_command('CoreNLP','java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000', 'stanford-corenlp-full-2016-10-31')
+    h.forever()
+start()

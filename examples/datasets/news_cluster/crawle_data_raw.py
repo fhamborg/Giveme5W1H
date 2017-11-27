c74f3b4429554c87d8b57dbed2fb551a7f35e23c
Benedikt Kromer <benediktkromer@gmail.com>
2017-11-27 13:36:31 +0100

    added readme and pre_processing to the new dataset

--- examples/datasets/news_cluster/crawle_data_raw.py
+++ examples/datasets/news_cluster/crawle_data_raw.py
@@ -1,4 +1,5 @@
 import enum
+import glob
 from enum import Enum, auto
 import hashlib
 import json
@@ -13,27 +14,7 @@ This is a dataset aiming to represent clustered news around topics.
 
 
 Used publisher:
-
-    Huffington
-        https://www.huffingtonpost.com/search?keywords=test+test&sortBy=recency&sortOrder=desc
-    CNN	
-        http://edition.cnn.com/search/?q=test
-    New York Times	
-        https://query.nytimes.com/search/sitesearch/#/test+test/
-    NPR	
-        https://www.npr.org/templates/search/index.php?searchinput=test+test
-    The Sun	
-        https://www.thesun.co.uk/?s=test
-    Daily Mail	
-       http://www.dailymail.co.uk/home/search.html?offset=0&size=50&sel=site&searchPhrase=test+test&sort=relevant&type=article&days=all
-    RT	
-        https://www.rt.com/search?q=test+test
-    The Hindu	
-        http://www.thehindu.com/search/?order=DESC&q=hokkaido+missile&sort=score
-    China Daily	
-        http://searchen.chinadaily.com.cn/search?query=hokkaido+missile
-    Spiegel	
-        http://www.spiegel.de/international/search/index.html?suchbegriff=test+test
+    see research_helper
 
 Used time horizon:
     2016-2017
@@ -43,27 +24,29 @@ Event Source
     http://eventregistry.org/search?type=events&query=%257B%2522categories%2522:%255B%257B%2522id%2522:145,%2522uri%2522:%2522dmoz%2FSports%2522,%2522label%2522:%2522Sports%2522,%2522negate%2522:false%257D,%257B%2522id%2522:380,%2522uri%2522:%2522dmoz%2FSociety%2FPolitics%2522,%2522label%2522:%2522Society%25E2%2586%2592Politics%2522,%2522negate%2522:false%257D,%257B%2522id%2522:19,%2522uri%2522:%2522dmoz%2FBusiness%2522,%2522label%2522:%2522Business%2522,%2522negate%2522:false%257D,%257B%2522id%2522:41,%2522uri%2522:%2522dmoz%2FScience%2FTechnology%2522,%2522label%2522:%2522Science%25E2%2586%2592Technology%2522,%2522negate%2522:false%257D%255D,%2522dateStart%2522:%25222016-01-01%2522,%2522dateEnd%2522:%25222016-12-31%2522,%2522lang%2522:%2522eng%2522,%2522minArticles%2522:200,%2522preferredLang%2522:%2522eng%2522%257D&tab=items&eventsSortBy=rel
     http://googlenews.com
     https://www.nexis.com/
+    https://en.wikipedia.org/wiki/Portal:Current_events/May_2016
 
 
 
 """
 
-
-
 @enum.unique
 class Category(Enum):
-    global_politics = auto()
-    economics = auto()
-    science_tech = auto()
+    global_politics = auto() # elections  # International relations # Armed conflicts and attacks
+    economics = auto() # Business
+    science_tech = auto() #  science
     entertainment = auto()
     sports = auto()
 
     # todo: add
-    # disaster
+    # disaster and accidents
+    # World
 @enum.unique
 class Topic(Enum):
     unspecific = auto()
     north_korea = auto()
+    hack = auto()
+
 @enum.unique
 class Event(Enum):
 
@@ -109,6 +92,7 @@ class Event(Enum):
     # Equifax_breach = auto() not happening at a specie time
     # Linkedin hack
     # Dropbox hack
+    hack_equifax_breach = auto()
 
 
     #
@@ -133,10 +117,6 @@ class Event(Enum):
 
     # Italy Earthquakes
 
-
-
-
-
 def write_json(path, filename, object):
     """
     writes an object to a path as json
@@ -147,6 +127,7 @@ def write_json(path, filename, object):
     with open(path + '/' + filename + '.' + 'json', 'w') as data_file:
         data_file.write(json.dumps(object, sort_keys=False, indent=2))
         data_file.close()
+
 
 def json_exist(path, filename):
     return os.path.exists(path + '/' + filename + '.' + 'json')
@@ -170,10 +151,43 @@ def add_article(a_category: Category, a_topic: Topic, a_Event: Event, urls: List
 
 #
 # raw data set
-#
+# add_article(Category.science_tech, Topic.hack, Event.hack_equifax_breach,  [])
+add_article(Category.science_tech, Topic.hack, Event.hack_equifax_breach,  [
+    "https://www.wsj.com/articles/equifax-earnings-drop-27-in-quarter-marred-by-cyberattack-1510268187",
+    "https://www.bloomberg.com/news/articles/2017-11-14/how-much-will-equifax-pay",
+    "https://gizmodo.com/equifax-seized-138-scammy-lookalike-domains-instead-of-1820450580",
+    'https://www.nytimes.com/2017/09/27/your-money/equifax-credit-freeze-lock-apology.html',
+    'https://www.nytimes.com/2017/10/02/business/equifax-breach.html',
+    'https://www.npr.org/sections/thetwo-way/2017/09/19/552124551/equifax-confirms-another-security-incident',
+    'https://www.thesun.co.uk/news/4425740/cyber-attack-equifax-data-stolen-brits/',
+    'https://www.thesun.co.uk/tech/4475843/equifax-admits-that-400000-british-peoples-private-info-was-accessed-during-major-cybersecurity-incident/',
+    'https://www.thesun.co.uk/news/4657635/credit-company-equifax-says-new-security-attack-means-that-almost-700000-brits-have-had-personal-details-stolen-by-cyber-hackers/',
+    'http://www.dailymail.co.uk/news/article-4942344/Equifax-2-5-million-Americans-affected-hack.html',
+    'http://www.dailymail.co.uk/news/article-4863648/Credit-agency-Equifax-info-143-million-stolen.html',
+    'https://www.rt.com/shows/boom-bust/402559-hurricane-harvey-irma-damage/'
+])
 
-# TODO not all of them refer to the same missile...
-add_article(Category.economics, Topic.north_korea, Event.north_Korea_hokkaido_missile, [
+
+add_article(Category.global_politics, Topic.unspecific, Event.las_vegas_shooting, [
+    'http://edition.cnn.com/videos/us/2017/10/02/vegas-witness-crowd-running.cnn',
+    'http://edition.cnn.com/2017/10/02/us/gallery/las-vegas-shooting/index.html',
+    'http://edition.cnn.com/videos/tv/2017/10/02/exp-tsr-elam-las-vegas-concert-mass-shooting.cnn',
+    'http://edition.cnn.com/videos/us/2017/10/02/donald-trump-entire-las-vegas-shooting-address-sot.cnn',
+    'https://www.nytimes.com/2017/10/02/us/stephen-paddock-vegas-shooter.html',
+    'https://www.nytimes.com/interactive/2017/10/02/us/mandalay-bay-vegas-shooting.html',
+    'https://www.rt.com/usa/405321-las-vegas-shooting-panic/',
+    'https://www.rt.com/shows/news-with-ed-schultz/405483-news-with-ed-october2/',
+    'https://www.rt.com/usa/405409-isis-responsibility-vegas-shooting/',
+    'http://www.thehindu.com/news/international/las-vegas-shooting-deadliest-in-us-several-killed-injured/article19787232.ece',
+    'http://www.thehindu.com/news/international/live-updates-las-vegas-strip-shooting/article19784319.ece',
+    'http://usa.chinadaily.com.cn/world/2017-10/03/content_32789696.htm',
+    'http://www.chinadaily.com.cn/cndy/2017-10/03/content_32789038.htm',
+    'http://usa.chinadaily.com.cn/world/2017-10/03/content_32788252.htm'
+
+])
+
+# TODO i think not all of them refer to the same missile/event...
+add_article(Category.global_politics, Topic.north_korea, Event.north_Korea_hokkaido_missile, [
     'https://www.huffingtonpost.com/entry/un-condemns-north-korea-missile-test_us_59a681e0e4b00795c2a2ba10',
     'https://www.huffingtonpost.com/entry/north-korea-missile-guam_us_59a5ff61e4b084581a14339c',
     'https://www.huffingtonpost.com/entry/north-korea-missile-japan_us_59a4874fe4b050afa90c1eab',
@@ -199,35 +213,52 @@ add_article(Category.economics, Topic.north_korea, Event.north_Korea_hokkaido_mi
 
 ])
 
-for index, info_article in enumerate(articles):
 
-    url = info_article['Url']
-    dId = hashlib.sha224(url.encode('utf-8')).hexdigest()
+if __name__ == '__main__':
+    # crawling itself
+    for index, info_article in enumerate(articles):
+        url = info_article['Url']
+        dId = hashlib.sha224(url.encode('utf-8')).hexdigest()
 
-    if not json_exist('data_raw', dId):
-        # this is an object
-        try:
-            article = NewsPlease.from_url(url)
+        if not json_exist('data_raw', dId):
+            # this is an object
+            try:
+                article = NewsPlease.from_url(url)
 
-            # this is an dict
-            article_dict = article.get_dict()
+                # this is an dict
+                article_dict = article.get_dict()
 
-            article_dict['newsCluster'] = info_article
+                article_dict['newsCluster'] = info_article
 
-            # enhancement for giveme5w
-            article_dict['dId'] = dId
+                # enhancement for giveme5w
+                article_dict['dId'] = dId
 
-            # datetime-datetime-not-json-serializable bugfix"
-            if article_dict.get('date_publish'):
-                article_dict['date_publish'] = article_dict['date_publish'].isoformat()
+                # datetime-datetime-not-json-serializable bugfix"
+                if article_dict.get('date_publish'):
+                    article_dict['date_publish'] = article_dict['date_publish'].isoformat()
 
-            write_json('data_raw', article_dict['dId'], article_dict)
+                write_json('data_raw', article_dict['dId'], article_dict)
+                print(url)
+            except Exception as e:
+                print(e)
+                print(url)
+        else:
             print(url)
-        except Exception as e:
-            print(e)
-            print(url)
-    else:
-        print(url)
-        print('skipped, already crawled')
+            print('skipped, already crawled')
 
 
+    # preprocess into data
+    for filepath in glob.glob('data_raw/*.json'):
+        with open(filepath, encoding='utf-8') as data_file:
+            data = json.load(data_file)
+            target = None
+
+            # giveme5w(and enhancer) needs at least these 3 filed to work proper
+            if data.get('date_publish') is not None and data.get('title') is not None and data.get('text') is not None:
+                target = 'data'
+            else:
+                target = 'data_damaged'
+
+            outfile = open(target + '/' + data['dId'] + '.json', 'w')
+            outfile.write(json.dumps(data, sort_keys=False, indent=2))
+            outfile.close()

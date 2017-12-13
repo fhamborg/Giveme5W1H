537156dab8ebd085b272067a89476e5b3d0d9e74
Benedikt Kromer <benediktkromer@gmail.com>
2017-12-13 22:05:17 +0100

    finalization for evaluation script: - find the weights with lowest error rate over all tested documents - find weights with the best (lowest) result over all documents - if their is an overlap -> save under golden_weights otherwise user has to select between lowest_error or  best_dist
    Lowest error is considering "no annotation to compare" as error, should be discussed.

--- examples/misc/learn_weights_new/evaluate.py
+++ examples/misc/learn_weights_new/evaluate.py
@@ -4,12 +4,12 @@ checks all result files an writes the best candidates to evaluate.json
 import glob
 import json
 import pickle
+import collections
 
 import statistics
 
+compare = lambda x, y: collections.Counter(x) == collections.Counter(y)
 
-def normalize(question, data):
-    pass
 
 
 def weights_to_string(weights):
@@ -41,10 +41,59 @@ def read_file(path):
 def remove_errors(list):
     return [x for x in list if x and x >= 0]
 
+def normalize(list):
+    """
+    this is assuming that there is a min with ß
+    :param list:
+    :return:
+    """
+    # find max
+    a_max = max(list)
+    # set errors to max
+    list_error_free = [x if x >= 0 else a_max for x in list]
+    # normalize
+    result = []
+    for entry in list_error_free:
+        result.append(entry/a_max)
+
+    return result
+
+
+def merge_top(a_list, accessor):
+    """
+    multiple weights can produce the same top-score, this function merges all top weights.
+    :param a_list: 
+    :param accessor: 
+    :return: 
+    """
+    result = a_list[0]
+    weights = []
+    result['weights'] = weights
+
+    for entry in a_list:
+        if entry[accessor] == result[accessor]:
+            weights.append(entry.get('weight'))
+        else:
+            return result
+
+def find_golden_weights(a_list):
+    """
+    checks for an overlap in  'lowest_error', 'best_dist'
+
+    :param results:
+    :return:
+    """
+    result = []
+    for lowest_error in a_list['lowest_error']['weights']:
+        for best_dist in a_list['best_dist']['weights']:
+            if compare(lowest_error, best_dist):
+                result.append(lowest_error)
+    a_list['golden_weights'] = result
+
 
 def index_of_best(list):
     """
-
+    low distance is better
     :param list:
     :return:
     """
@@ -71,9 +120,10 @@ if __name__ == '__main__':
         for combination_string in score_results[question]:
             combo = score_results[question][combination_string]
 
-            scores_cleaned = remove_errors(combo['scores_doc'])
-            errors = len(combo['scores_doc']) - len(scores_cleaned)
-            a_sum = sum(scores_cleaned)
+            raw_scores = combo['scores_doc']
+            scores_cleaned = remove_errors(raw_scores)
+            errors = len(raw_scores) - len(scores_cleaned)
+            a_sum = sum(normalize(raw_scores))
 
             combo['avg'] = a_sum / len(scores_cleaned)
             score_per_average.setdefault(question,{})[combination_string] = {
@@ -87,21 +137,7 @@ if __name__ == '__main__':
                 'weight': combo['weights']
             }
 
-    for question in results_error_rate:
-        print(question)
-        results_error_rate_list = list(results_error_rate[question].values())
-        score_per_average_list = list(score_per_average[question].values())
-
-        results_error_rate_list.sort(key=lambda x: x['errors'], reverse=False)
-        score_per_average_list.sort(key=lambda x: x['avg'], reverse=False)
-
-        print('lowest error rate:')
-        print(json.dumps(results_error_rate_list[:30], sort_keys=False, indent=4))
-
-        print('best scoring weights:')
-        print(json.dumps(score_per_average_list[:30], sort_keys=False, indent=4))
-
-    # nice formattet full output, if anyone need is
+            # nice formattet full output, if anyone needs is
     nice_format = {}
     for question in score_results:
         question_scores = nice_format.setdefault(question, [])
@@ -113,3 +149,30 @@ if __name__ == '__main__':
     with open('result/evaluation_only_avg' + '.json', 'w') as data_file:
         data_file.write(json.dumps(nice_format, sort_keys=False, indent=4))
         data_file.close()
+
+    # finally, get the best weighting and dave it to a file
+    final_result = {}
+    for question in results_error_rate:
+
+        results_error_rate_list = list(results_error_rate[question].values())
+        score_per_average_list = list(score_per_average[question].values())
+
+        results_error_rate_list.sort(key=lambda x: x['errors'], reverse=False)
+        score_per_average_list.sort(key=lambda x: x['avg'], reverse=False)
+
+
+
+
+        final_result[question] = {
+            'lowest_error': merge_top(results_error_rate_list, 'errors'),
+            'best_dist': merge_top(score_per_average_list, 'avg')
+        }
+        find_golden_weights(final_result[question])
+
+
+    print(json.dumps(final_result, sort_keys=False, indent=4))
+    with open('result/final_result_avg' + '.json', 'w') as data_file:
+        data_file.write(json.dumps(final_result, sort_keys=False, indent=4))
+        data_file.close()
+
+

741c266b81a8497571497bd3eaa14bfa6b301d55
Felix Hamborg <felix@hamborg.eu>
2018-06-28 16:55:34 +0200

    update stanford corenlp version in startup scripts

--- examples/startup/environment_with_enhancer.py
+++ examples/startup/environment_with_enhancer.py
@@ -5,7 +5,7 @@ def start():
     h = StartupHelper()
     h.do_command('CoreNLP',
                  'java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000',
-                 'stanford-corenlp-full-2016-10-31')
+                 'stanford-corenlp-full-2017-06-09')
 
     h.do_command('AIDA', 'export MAVEN_OPTS="-Xmx16G" && mvn jetty:run', 'aida-3.0.4')
 
